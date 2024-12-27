8769e6dc975f6326846013fa1843819c4a17915c
Blanquette0315 <tjfwn0315@gmail.com>
2024-12-27 10:22:33 +0900

    Code

--- Solo Study/최댓값 최솟값.cpp
+++ Solo Study/최댓값 최솟값.cpp
@@ -10,7 +10,7 @@ string solution(string s) {
     int imax = -0x3f3f3f3f;;
     int imin = 0x3f3f3f3f;;
 
-    /*while (s.find(' ') != string::npos)
+    while (s.find(' ') != string::npos)
     {
         string str_num = s.substr(0, s.find(' '));
         int num = stoi(str_num);
@@ -19,19 +19,7 @@ string solution(string s) {
         imin = min(imin, num);
 
         s.erase(0, s.find(' ') + 1);
-    }*/
-
-    while (!s.empty())
-    {
-        string str_num = s.substr(0, s.find(' '));
-        int num = stoi(str_num);
-
-        imax = max(imax, num);
-        imin = min(imin, num);
-        
-        s.erase(s.rfind(' '));
     }
-
 
     answer = to_string(imin) + ' ' + to_string(imax);
 
