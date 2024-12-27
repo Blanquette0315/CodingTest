789e00123f5d04675c9b46c03635915be0762d16
Blanquette0315 <tjfwn0315@gmail.com>
2024-12-27 10:54:27 +0900

    code

---
+++ Solo Study/올바른 괄호.cpp
@@ -0,0 +1,32 @@
+#include<string>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+
+    stack<char> st;
+
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (s[i] == '(')
+            st.push(s[i]);
+        else
+            if (st.empty() || st.top() == ')')
+            {
+                answer = false;
+                break;
+            }
+            else
+            {
+                st.pop();
+            }
+    }
+
+    if (!st.empty())
+        answer = false;
+
+    return answer;
+}
