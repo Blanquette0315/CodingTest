45f88e57d158f05cd92bc7548bca0155b520f6f8
Blanquette0315 <tjfwn0315@gmail.com>
2024-12-31 10:17:15 +0900

    2024.12.31

---
+++ Solo Study/부족한 금액 계산하기.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = -1;
+
+    long long pay = 0;
+    for (int i = 1; i <= count; ++i)
+    {
+        pay += price * i;
+    }
+
+    if (pay > money)
+        answer = pay - money;
+    else
+        answer = 0;
+
+    return answer;
+}
