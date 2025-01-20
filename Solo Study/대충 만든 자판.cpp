c9aa561cfdee7cebbdf2e3404f6638fa7274ac90
Blanquette0315 <tjfwn0315@gmail.com>
2025-01-20 09:38:33 +0900

    2025.01.20

---
+++ Solo Study/대충 만든 자판.cpp
@@ -0,0 +1,67 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> keymap, vector<string> targets) {
+    vector<int> answer;
+
+    map<char, int> map_Key;
+
+    for (int i = 0; i < keymap.size(); ++i)
+    {
+        for (int j = 0; j < keymap[i].size();++j)
+        {
+            if (map_Key.find(keymap[i][j]) == map_Key.end())
+            {
+                map_Key.insert({ keymap[i][j], j + 1 });
+            }
+            else
+            {
+                if (map_Key.find(keymap[i][j])->second > i + 1)
+                {
+                    map_Key.find(keymap[i][j])->second = i + 1;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < targets.size(); ++i)
+    {
+        bool none = false;
+        int cnt = 0;
+        for (int j = 0; j < targets[i].size(); ++j)
+        {
+            if (map_Key.find(targets[i][j]) == map_Key.end())
+            {
+                none = true;
+                break;
+            }
+            else
+            {
+                cnt += map_Key.find(targets[i][j])->second;
+            }
+        }
+
+        if (none)
+        {
+            answer.push_back(-1);
+            none = false;
+            continue;
+        }
+        else
+        {
+            answer.push_back(cnt);
+        }
+    }
+
+    return answer;
+}
+
+void main()
+{
+    vector<int> answer = solution({ "ABACD", "BCEFD" }, { "ABCD", "AABB" });
+
+    return;
+}
